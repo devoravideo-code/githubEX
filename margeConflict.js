2da235ba31e9ae06384b4d788547bea6490d65d6
Devora Wexler <devora.video@gmail.com>
2025-11-05 20:36:01 +0200

    delete func3 in main

--- margeConflict.js
+++ margeConflict.js
@@ -6,7 +6,4 @@ const func2 = ()=>{
     console.log("Function 2");
 }
 
-const func3 = ()=>{
-    //the line that was here deleted by branch feature-2
-    console.log("Additional line in func3");
-}
+//the func that was here deleated by the main branch
