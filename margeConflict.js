f59ff15ce3193115a103576ea30de2280f34c85c
YaeliP <porat4241@gmail.com>
2025-11-11 19:40:53 +0200

    change without conflict

--- margeConflict.js
+++ margeConflict.js
@@ -2,14 +2,8 @@ const func1 = ()=>{
     console.log("Function 1");
 }
 
-const func2 = ()=>{
-    console.log("Function 2");
-}
 
-const func3 = () =>{
-    console.log("function 3");
-    
-}
+
 
 const func4 = () =>{
     console.log("function 4");
